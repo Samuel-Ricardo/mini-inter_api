552c91aa8edfbcce0f83dfc2a30ba524461318e0
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-12-18 09:20:33 -0300

    Update: File - messages.ts (./config)

--- src/config/messages.ts
+++ src/config/messages.ts
@@ -1,6 +1,12 @@
-export const messages = {
-  ERROR: {
-    USER_NOT_FOUND: 'User not found',
+export const error = {
+  USER_NOT_FOUND: 'User not found',
     USER_ALREADY_EXISTS: "There is already a registered user with this email"
-  }
 }
+
+const messages = {
+  ERROR: {...error}
+};
+
+export default messages;
+
+
