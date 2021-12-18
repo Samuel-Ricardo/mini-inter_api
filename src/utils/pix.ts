317f9588ecab597373aec2bdd3ff0005130cd82f
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-12-17 23:39:14 -0300

    Create: Function - encodeKey - (./utils/pix.ts)

--- src/utils/pix.ts
+++ src/utils/pix.ts
@@ -1,3 +1,11 @@
 import {encode, decode} from 'js-base64'
 
- 
+export const encodeKey = (userID: string, value: number, registerID: string) => {
+
+  const stage1 = encode(userID);
+  const stage2 = encode(value.toString());
+  const stage3 = encode(registerID);
+
+  return `${stage1}-${stage2}-${stage3}`
+}
+
