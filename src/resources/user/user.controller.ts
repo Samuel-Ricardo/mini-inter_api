345c7fb3e74be43eb3a51d204039f88160291751
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-12-18 09:34:54 -0300

    Create: Function - singnup (./resources/user/user.controller.ts)

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -11,4 +11,10 @@ export default class UserController {
 
     return res.status(200).send(users);
   }
+
+  async singnup(req: Request, res: Response) {
+    const userService = new UserService();
+    const users = await userService.signup(req.body);
+    return res.status(201).send(users);
+  }
 }
