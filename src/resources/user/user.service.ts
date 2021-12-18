ab740f77bf6b812a700c274ef759e3b76be9b264
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-12-18 00:03:47 -0300

    Create: Class - UserService (./resources/user/user.service.ts)

---
+++ src/resources/user/user.service.ts
@@ -0,0 +1,11 @@
+import { getRepository } from "typeorm";
+import { sign } from "jsonwebtoken";
+import md5 from "crypto-js";
+import { UserSignIn } from "./DTOS/user.signin.dtos";
+import { User } from "../../entity/User";
+
+
+
+export default class UserService {
+
+}
