0a93ba9bf1a60823140046e1098c12192af641d4
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-12-18 09:31:13 -0300

    Create: Function - singnin (./resources/user/user.controller.ts)

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -3,5 +3,12 @@ import UserService from "./user.service";
 
 export default class UserController {
 
-  
+  async singin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const userService = new UserService();
+    const users = await userService.sigin({ email, password });
+
+    return res.status(200).send(users);
+  }
 }
