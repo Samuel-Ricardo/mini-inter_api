58e8eda0e50f71181f4645006833d88db0e30224
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-01 19:33:52 -0300

    Update: messages

--- src/config/messages.ts
+++ src/config/messages.ts
@@ -1,17 +1,20 @@
 
-export const pix = {
-  RECEIVE_PIX_FROM_SAME_USER: 'It is not possible to receive the PIX from the same user'
-}
+
 
 export const error = {
   USER_NOT_FOUND: 'User not found',
   USER_ALREADY_EXISTS: "There is already a registered user with this email",
-  PIX: { ...pix }
+  PIX: { ...pix_error }
+}
+
+export const success = {
+
+  PIX: {...pix_success}
 }
 
 const messages = {
   ERROR: { ...error },
-
+  SUCCESS: {...success}
 };
 
 export default messages;
