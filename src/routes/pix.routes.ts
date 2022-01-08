cd1cf1f76f7b577f237c4bdb34ec8fea86fdbf3a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 18:13:05 -0300

    Created: Routes - pix (./routes/pix.routes.ts )

--- src/routes/pix.routes.ts
+++ src/routes/pix.routes.ts
@@ -1,1 +1,7 @@
+import { Router } from "express";
+import PixController from "../resources/pix/pix.controller";
 
+const pixRoutes = Router();
+const pixController = new PixController();
+
+export default pixRoutes;
