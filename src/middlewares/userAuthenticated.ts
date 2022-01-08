67b64c51f95fdde7c4b77aff0d433ce5af0dedc9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 18:54:36 -0300

    Created: Middleware - user authenticated (./middlewares)

--- src/middlewares/userAuthenticated.ts
+++ src/middlewares/userAuthenticated.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from "express";
 
 interface ITokenPayload{
   firstName: string;
@@ -5,4 +6,12 @@ interface ITokenPayload{
   iat: number;
   exp: number;
   sub: string;
+}
+
+export default function userAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+
 }
