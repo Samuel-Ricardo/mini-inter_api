5897742a72adce5f4b7d6b8fd37ffe094efdeaf8
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 20:01:16 -0300

    Update: File - index.ts (./)

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,19 @@ import { globalErros } from "./middlewares/globalErros";
 
 createConnection().then(Connection => {
 
-  
+  const app = express();
+  const PORT = 3333;
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
+
+  app.use(globalErros);
+
+  app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+  });
+
 }).catch((error) => {
   console.log("Unable to connect to the database", error);
 })
