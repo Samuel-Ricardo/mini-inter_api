4e136ddf33e391ea31f975288d31ebdea5296f14
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 18:08:34 -0300

    Created: Routes - user (./routes/user.routes.ts )

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,1 +1,9 @@
+import { Router } from "express";
+import UserController from "../resources/user/user.controller";
 
+const userRouter = Router();
+const userController = new UserController();
+
+
+
+export default userRouter;
