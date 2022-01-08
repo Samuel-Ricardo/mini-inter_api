f62c535a4b1f2e50c8f1bfcfc99375cd1d542d9e
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 17:40:03 -0300

    Created: Function - request (.../pix.controller.ts)

--- src/resources/pix/pix.controller.ts
+++ src/resources/pix/pix.controller.ts
@@ -1,4 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import PixService from "./pix.service";
 
 export default class PixController {
+
+  async request(req: Request, res: Response, next: NextFunction) {
+
+    const pixService = new PixService();
+
+    const { value } = req.body;
+    const user = req.user;
+
+    const requestKey = await pixService.request(value, user);
+    return res.status(200).send({ copyPasteKey: requestKey });
+  }
 
 }
