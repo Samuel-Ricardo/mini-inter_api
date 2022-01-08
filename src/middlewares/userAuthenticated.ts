b72122f391c3e2b9e9107e9ee86e81502718da22
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 19:41:46 -0300

    Middleware: Check - Token

--- src/middlewares/userAuthenticated.ts
+++ src/middlewares/userAuthenticated.ts
@@ -1,8 +1,11 @@
+import { verify } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
+import auth from "../config/auth";
+import AppError from "../shared/error/AppError";
 
 interface ITokenPayload{
-  firstName: string;
-  lastName: string;
+  first_name: string;
+  last_name: string;
   iat: number;
   exp: number;
   sub: string;
@@ -14,4 +17,24 @@ export default function userAuthenticated(
   next: NextFunction,
 ): void {
 
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) throw new AppError('JWT Não enviado', 401);
+
+  const [, token] = authHeader.split('');
+
+  try {
+    const decoded = verify(token, auth.jwt.secret);
+    const { sub, first_name, last_name } = decoded as ITokenPayload;
+
+    req.user = {
+      id: sub,
+      first_name,
+      last_name
+    }
+
+    return next();
+  } catch {
+    throw new AppError('Token JWT Inválido', 401);
+  }
 }
