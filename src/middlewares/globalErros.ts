ed7f703e23c9fb3c8b930204869d266465074a78
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 18:45:04 -0300

    Created: Middleware - global erros (./middlewares/globalErros.ts)

---
+++ src/middlewares/globalErros.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+
+function globalErros(err:Error, request:Request, response:Response, next:NextFunction){
+
+
+}
+
+export { globalErros };
