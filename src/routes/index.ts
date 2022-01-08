e4b94a22d06479dc064f77f2e2b2ed941df2ed84
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 18:19:16 -0300

    Merge: Routes - base + user (./routes/index.ts)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import pixRoutes from "./pix.routes";
+import userRouter from "./user.routes";
 
 const routes = Router();
+
+routes.use('/user', userRouter);
+
 
 export default routes;
