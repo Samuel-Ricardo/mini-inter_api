18ef4f004deed926b0fe82ef943e1d3824884e04
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-08 18:09:04 -0300

    Created: Routes - /singin (./routes/user.routes.ts )

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,6 +4,7 @@ import UserController from "../resources/user/user.controller";
 const userRouter = Router();
 const userController = new UserController();
 
+userRouter.post('/singin', userController.singin);
 
 
 export default userRouter;
