7d12b5276d9437aac77592525d248f5c85a32ea5
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-27 12:02:06 -0300

    use auth middleware

--- src/routes/pix.routes.ts
+++ src/routes/pix.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import userAuthenticated from "../middlewares/userAuthenticated";
 import PixController from "../resources/pix/pix.controller";
 
 const pixRoutes = Router();
 const pixController = new PixController();
+
+pixRoutes.use(userAuthenticated);
 
 pixRoutes.post('/request', pixController.request);
 pixRoutes.post('/pay/:key', pixController.pay);
